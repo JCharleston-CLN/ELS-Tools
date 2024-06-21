49540bc680d5a5396e1728348661c78b45e287ae
JCharleston-CLN <58193978+JCharleston-CLN@users.noreply.github.com>
2024-06-20 21:39:14 -0400

    Update artifactory-tool.sh

--- artifactory-tool.sh
+++ artifactory-tool.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# URL of the encrypted executable
+# URL of the encrypted executable (raw URL)
 URL="https://github.com/JCharleston-CLN/ELS-Tools/raw/main/artifactory-tool.sh.x"
 
 # Download the encrypted executable
-curl -O $URL
+wget $URL -O artifactory-tool.sh.x
 
 # Set execute permission
 chmod +x artifactory-tool.sh.x
