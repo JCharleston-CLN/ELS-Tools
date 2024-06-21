be1ff4459858b5109379290bbdf28cf9cb89ece9
JCharleston-CLN <58193978+JCharleston-CLN@users.noreply.github.com>
2024-06-20 21:02:02 -0400

    Create artifactory-tool.sh

---
+++ artifactory-tool.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# URL of the encrypted executable
+URL="https://github.com/JCharleston-CLN/ELS-Tools/blob/main/artifactory-tool.sh.x"
+
+# Download the encrypted executable
+curl -O $URL
+
+# Set execute permission
+chmod +x artifactory-tool.sh.x
+
+# Execute the script
+./artifactory-tool.sh.x
